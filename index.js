6eedbb701b29a77d86aaef72566bbb8c1ac69ade
tech master <bryan77@twc.com>
2019-07-10 06:45:22 -0400

    add Machine component, jsx, logic, and props

---
+++ index.js
@@ -0,0 +1,27 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Chance Machine!</h1>
+                <Machine 
+                    s1="🍒"
+                    s2="🍒"
+                    s3="🍒"
+                />
+                <Machine 
+                    s1="🍒"
+                    s2="🍒"
+                    s3="🍊"
+                />
+                <Machine 
+                    s1="🍊"
+                    s2="🍓"
+                    s3="🍒"
+                />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
